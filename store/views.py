433b55106eb8d2ae176fce9732210891c74a07ca
William Boehringer <williamboehringer1@gmail.com>
2023-01-19 17:14:53 -0300

    add/edit/delete products sysyem implemented

--- store/views.py
+++ store/views.py
@@ -5,16 +5,16 @@ from .models import Product, Category
 
 def category_detail(request, slug):
     category = get_object_or_404(Category, slug=slug)
-    products = category.products.all()
+    products = category.products.filter(status=Product.ACTIVE)
     return render(request, 'store/category_detail.html', {'category': category, 'products': products})
 
 def product_detail(request, category_slug, slug):
-    product = get_object_or_404(Product, slug=slug)
+    product = get_object_or_404(Product, slug=slug, status=Product.ACTIVE)
     return render(request, 'store/product_detail.html', {'product': product})
 
 def search(request):
      query = request.GET.get('query', '')
-     products = Product.objects.filter(Q(title__icontains=query)
+     products = Product.objects.filter(status=Product.ACTIVE).filter(Q(title__icontains=query)
      | Q(category__title__icontains=query)
      | Q(description__icontains=query))
 
--- users/urls.py
+++ users/urls.py
@@ -3,9 +3,14 @@ from users import views
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-    path('vendor/<int:pk>/', views.vendor_detail, name='vendor_detail'),
-    path('myaccount/', views.myaccount, name='myaccount'),
     path('signup/', views.signup, name='signup'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='logout'),
+    path('vendor/<int:pk>/', views.vendor_detail, name='vendor_detail'),
+    path('my-store/', views.my_store, name='my_store'),
+    path('my-store/add-products/', views.add_product, name='add_product'),
+    path('my-store/edit-product/<int:pk>/', views.edit_product, name='edit_product'),
+    path('my-store/delete-product/<int:pk>/', views.delete_product, name='delete_product'),
+    path('myaccount/', views.myaccount, name='myaccount'),
+    
 ]
